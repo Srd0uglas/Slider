4e99845808b4f4da7d754d46bf1c39fbc9d31617
Douglas Souza <srd0uglas337@gmail.com>
2024-06-27 13:32:41 -0300

    finalizando o js e começando as animações da página

--- assests/script.js
+++ assests/script.js
@@ -16,10 +16,34 @@ btnBack.onclick = () => moveItemsOnclick('back')
 // TYPE: DEFINIR SE É NEXT OU BACK
          // o move ou back entra dentro desse parenteses
 function moveItemsOnclick(type){
-    // se for "next" vai fazer uma determinada ação
-    if(type === 'next'){
-        
-    }else{
+    let listItems = document.querySelectorAll('.list .list-item')
+    let thumbItems = document.querySelectorAll('.thumb .thumb-item')
 
+
+    // se for "next" vai fazer uma determinada ação
+      // se eu clicar no botão next
+    if(type === 'next'){
+        // appendChild coloca o item no começo da lista
+        list.appendChild(listItems [0])
+        thumb.appendChild(thumbItems [0])
+        // vamos usar uma classListserve para adicionar uma classe temporária 
+        container.classList.add('next')
+    }else{
+        // prepend coloca o item no final da lista
+        list.prepend(listItems[3])
+        thumb.prepend(thumbItems[3])
+        container.classList.add('back')
     }
+    // as fotos tem 4 posições = 0 1 2 3 
+
+    
+
+
+    // vai fazer a animação que está definida na classe add ou back e vai ser excluida após um determinado tempo
+    setTimeout(() =>{
+        // remover as classes após 3s
+        container.classList.remove('next')
+        container.classList.remove('back')
+    },3000); 
+    // 3 seg = 3000 pois está em milesegundos
 }
